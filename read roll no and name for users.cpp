e47727c82f79f947ca9cd2b6a480ef790c41df75
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2021-10-20 21:17:30 +0530

    Create read roll no and name for users.cpp

---
+++ read roll no and name for users.cpp
@@ -0,0 +1,18 @@
+/* write a c++ program on read your roll no and name to keyboard*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int rno;
+    char name[20];
+    cout<<"enter the rollno:";
+    cin>>rno;
+    cout<<"enter the name:";
+    cin>>name;
+    cout<<"rollno:"<<rno;
+    cout<<"name:"<<name;
+}
