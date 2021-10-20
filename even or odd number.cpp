7ffdfe05bddf25961cff6638daa59871e173c0be
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2021-10-20 21:23:36 +0530

    Create even or odd number.cpp

---
+++ even or odd number.cpp
@@ -0,0 +1,15 @@
+/*write a c++ program on to check the given number is even or odd*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"enter the number:";
+	cin>>num;
+	if((num%2)==0)
+	cout<<"the number is even";
+	else
+	cout<<"the number is odd";
+}
