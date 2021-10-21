909780f820a3332172931cb00ce723a2f8a4e298
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2021-10-21 20:55:25 +0530

    Create basic datatype1.cpp

---
+++ basic datatype1.cpp
@@ -0,0 +1,23 @@
+/*data types */
+
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c[20];
+    float d;
+    double e;  
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
