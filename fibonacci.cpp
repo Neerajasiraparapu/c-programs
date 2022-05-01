424296477c514a0f9e7b4fd2795cc2d7df582c3d
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-01 16:08:46 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=0,b=1,c,n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+            cout<<a<<" ";
+            c=a+b;
+            a=b;
+            b=c;
+        }
+}
