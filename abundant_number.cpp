1d29550f6ea9b36a93ce831ffba70bd2fa519168
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-12 10:47:11 +0530

    Create abundant_number.cpp

---
+++ abundant_number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,sum=1;
+    cin>>n;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            sum+=i;
+        }
+    }
+    if(sum>n){
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+}
